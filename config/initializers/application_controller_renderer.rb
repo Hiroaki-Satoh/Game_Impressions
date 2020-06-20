6727f79db6c63b3cc99ebec063d4111154b4d16d
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-06-20 15:56:08 +0900

    set Refile.secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# Refile.secret_key 情報の設定
+Refile.secret_key = 'f48fec73d70061ee651b5bd3db7bff4c892c1d3b08af1d9ee95b7d886d5c609a6fbdacc00c8b244417b8e2831118456e8620e181559bad9ce68a94a97c8d86ea'
