d8dc09c38499f094c823937dba0ffb48a2cc69a7
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-06-22 02:38:48 +0900

    fix user::impressionsController

--- app/controllers/user/impressions_controller.rb
+++ app/controllers/user/impressions_controller.rb
@@ -1,6 +1,6 @@
 class User::ImpressionsController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
-  before_action :set_user, expext: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_user, except: [:index, :show]
 
   def new
   	@impression = Impression.new
