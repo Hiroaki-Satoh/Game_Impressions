463e053b3e7a1e29a0448dd7ed567f9286d5e8c6
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-06-24 23:51:38 +0900

    レビュー機能の作成

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -1,2 +1,31 @@
 class User::ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+  	@game_title = GameTitle.find(params[:game_title_id])
+  	@review = current_user.reviews.new(review_params)
+  	@review.game_title_id = @game_title.id
+  	if @review.save
+  	  flash[:success] = "レビューを投稿しました！"
+  	  redirect_back(fallback_location: root_url) # 1つ前のURL（game_title詳細）へ
+  	end
+  end
+
+  def edit
+  	@review = Review.find(params[:game_title_id])
+  end
+
+  def update
+  	@review = Review.find(params[:game_title_id])
+  	if @review.update
+  	  flash[:notice] = "レビューを更新しました！"
+  	  redirect_back(fallback_location: root_url) # 1つ前のURL（game_title詳細）へ
+  	end
+  end
+
+  private
+  def review_params
+  	params.require(:review).permit(:title, :body, :graphic,
+  		                            :sound, :story, :volume, :recommended)
+  end
 end
