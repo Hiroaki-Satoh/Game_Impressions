b14535a5351ff3e8e4b5bbb4571f3e62af633066
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-09-13 18:08:12 +0900

    modified Admin::GameTitleController

--- app/controllers/admin/game_titles_controller.rb
+++ app/controllers/admin/game_titles_controller.rb
@@ -1,37 +1,41 @@
 class Admin::GameTitlesController < ApplicationController
   before_action :authenticate_admin!
 
+  # ゲームタイトル一覧ページの表示
   def index
-  	@game_title = GameTitle.new
-  	# 降順で1ページにつき10個表示させる。
-  	# 降順 order("カラム名 DESC"), 昇順 order("カラム名 ABC")
+  	@game_title = GameTitle.new # ゲームタイトル新規作成の為
   	@game_titles = GameTitle.all.order("id DESC").page(params[:page]).per(10)
+    # 降順 order("カラム名 DESC"), 昇順 order("カラム名 ABC")
   end
 
+  # ゲームタイトル詳細ページの表示
   def show
   	@game_title = GameTitle.find(params[:id])
   end
 
+  # ゲームタイトル新規作成
   def create
   	@game_title = GameTitle.new(game_title_params)
   	if @game_title.save
-  	  redirect_to admin_game_title_path(@game_title.id) # 作成したゲームタイトル詳細ページへ
+  	  redirect_to admin_game_title_path(@game_title.id)
   	else
   	  @game_titles = GameTitle.all.page(params[:page]).per(10)
-  	  render :index # 新規作成&一覧ページへ
+  	  render :index
   	end
   end
 
+  # ゲームタイトル編集ページの表示
   def edit
   	@game_title = GameTitle.find(params[:id])
   end
 
+  # ゲームタイトル情報の更新
   def update
   	@game_title = GameTitle.find(params[:id])
   	if @game_title.update(game_title_params)
-  	  redirect_to admin_game_title_path(@game_title.id) # 変更したゲームタイトル詳細ページへ
+  	  redirect_to admin_game_title_path(@game_title.id)
   	else
-  	  render :edit # 編集ページへ
+  	  render :edit
   	end
   end
 
